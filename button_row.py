2a6949c13a9bf010cb4d1b6810f8fa132f1a144a
d3m1human <ajin.ajinv@gmail.com>
2019-02-04 19:20:49 +0530

    menuupdate

--- button_row.py
+++ button_row.py
@@ -4,21 +4,29 @@ from tkinter import *
 
 def framereturn(root):
     frame1 = Frame(root)
-    b1 = Button(frame1, text="BASS")
+    frame1.config(bg="white")
+    b1 = Button(frame1, text="BASS",width=10)
     b1.pack(side=LEFT)
-    b2 = Button(frame1, text="SNEAR")
-    b2.pack(side=LEFT)
-    b3 = Button(frame1, text="HIGH HAT")
-    b3.pack(side=LEFT)
+    b2 = Button(frame1, text="SNEAR",width=10)
+    b2.pack(side=LEFT,padx=10,pady=20)
 
+    b3 = Button(frame1, text="HIGH HAT",width=10)
+    b3.pack(side=LEFT)
     return frame1
 
 def bottomframe(root):
-    bframe=Frame(root)
-    b1=Button(bframe,text="MODE")
-    b1.pack(side=LEFT)
-    b2=Button(bframe,text="RESET")
-    b2.pack(side=RIGHT)
+    bframe = Frame(root)
+    bframe.config(bg="white")
+    b1 = Menubutton(bframe, text="MODE")
+    b1.menu = Menu(b1, tearoff=0)
+    b1["menu"] = b1.menu
+    TBLAVar=IntVar()
+    JAZZVar=IntVar()
+    b1.menu.add_checkbutton(label="TABALA",variable=TBLAVar)
+    b1.menu.add_checkbutton(label="JAZZ", variable=JAZZVar)
+    b1.pack(side=LEFT,padx=50)
+    b2 = Button(bframe, text="RESET")
+    b2.pack(side=RIGHT,padx=50)
     return bframe
 
 
