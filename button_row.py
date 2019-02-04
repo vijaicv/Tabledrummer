86d25f04423a425d98793e39962f220ce4ac3721
d3m1human <ajin.ajinv@gmail.com>
2019-02-04 15:10:05 +0530

    frame1

--- button_row.py
+++ button_row.py
@@ -1,11 +1,16 @@
-from tkinter import Button, LEFT
-from graph_display import *
 
 
-b1=Button(frame1,text='BASS')
-b2=Button(frame1,text='SNEAR')
-b3=Button(frame1,text='HIGH HAT')
+from tkinter import *
 
+def framereturn(root):
+    frame1 = Frame(root)
+    b1 = Button(frame1, text="BASS")
+    b1.pack(side=LEFT)
+    b2 = Button(frame1, text="SNEAR")
+    b2.pack(side=LEFT)
+    b3 = Button(frame1, text="HIGH HAT")
+    b3.pack(side=LEFT)
+    return frame1
 
 
 
