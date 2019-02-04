87b92f24c616a43dd2ccf853bc51776e2182c5ec
d3m1human <ajin.ajinv@gmail.com>
2019-02-04 15:24:45 +0530

    bottomframe

--- button_row.py
+++ button_row.py
@@ -10,9 +10,16 @@ def framereturn(root):
     b2.pack(side=LEFT)
     b3 = Button(frame1, text="HIGH HAT")
     b3.pack(side=LEFT)
+
     return frame1
 
-
+def bottomframe(root):
+    bframe=Frame(root)
+    b1=Button(bframe,text="MODE")
+    b1.pack(side=LEFT)
+    b2=Button(bframe,text="RESET")
+    b2.pack(side=RIGHT)
+    return bframe
 
 
 
