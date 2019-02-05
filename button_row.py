7aacaae3d35bdf1f19dbe529194efd562e2ed12c
d3m1human <ajin.ajinv@gmail.com>
2019-02-05 10:07:28 +0530

    instrument modified

--- button_row.py
+++ button_row.py
@@ -7,26 +7,25 @@ def framereturn(root):
     frame1.config(bg="white")
     b1 = Button(frame1, text="BASS",width=10)
     b1.pack(side=LEFT)
+    b1.config(bd=5,relief=RAISED)
     b2 = Button(frame1, text="SNEAR",width=10)
     b2.pack(side=LEFT,padx=10,pady=20)
-
+    b2.config(bd=5, relief=RAISED)
     b3 = Button(frame1, text="HIGH HAT",width=10)
     b3.pack(side=LEFT)
+    b3.config(bd=5, relief=RAISED)
     return frame1
 
 def bottomframe(root):
     bframe = Frame(root)
     bframe.config(bg="white")
-    b1 = Menubutton(bframe, text="MODE")
-    b1.menu = Menu(b1, tearoff=0)
-    b1["menu"] = b1.menu
-    TBLAVar=IntVar()
-    JAZZVar=IntVar()
-    b1.menu.add_checkbutton(label="TABALA",variable=TBLAVar)
-    b1.menu.add_checkbutton(label="JAZZ", variable=JAZZVar)
-    b1.pack(side=LEFT,padx=50)
+    b1=StringVar(root)
+    b1.set('JAZZ')
+    instrument=OptionMenu(root,b1,"JAZZ","TABALA")
+    instrument.pack()
     b2 = Button(bframe, text="RESET")
-    b2.pack(side=RIGHT,padx=50)
+    b2.config(bd=4,relief=RAISED)
+    b2.pack(side=RIGHT,pady=10)
     return bframe
 
 
