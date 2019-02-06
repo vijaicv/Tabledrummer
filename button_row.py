4d8ea0ced2aff91f4fc388b39804d34bd3403d6e
d3m1human <ajin.ajinv@gmail.com>
2019-02-06 12:07:40 +0530

    sounds added

--- button_row.py
+++ button_row.py
@@ -38,4 +38,3 @@ class b_row:
 
 
 
-
--- sound_play.py
+++ sound_play.py
@@ -1,31 +1,48 @@
-##import pyaudio
-##import wave
-##import sys
-##CHUNK = 1024
-##wf= wave.open("D:\jazz\snare",'rb')
-##p= pyaudio.PyAudio()
-##stream= p.open(format=p.get_format_from_width(wf.getsampwidth()),
-##               channels=wf.getnchannels(),
-##               rate=wf.getframerate(),
-##               output=True)
-##data=wf.readframes(CHUNK)
-##
-##while len(data)>0:
-##    stream.write(data)
-##    data=wf.readframes(CHUNK)
-##
-##stream.stop_stream()
-##stream.close()
-##
-##p.terminate
+import pyaudio
+import wave
+import sys
+# def play_file(fname):
+#     # create an audio object
+#     wf = wave.open("/home/demihuman/Documents/project/Tabledrummer/samples/909_snr2.wav", 'rb')
+#     p = pyaudio.PyAudio()
+#     chunk = 1024
+#
+#     # open stream based on the wave object which has been input.
+#     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#                     channels=wf.getnchannels(),
+#                     rate=wf.getframerate(),
+#                     output=True)
+#
+#     # read data (based on the chunk size)
+#     data = wf.readframes(chunk)
+#
+#     # play stream (looping from beginning of file to the end)
+#     while data != '':
+#         # writing to the stream is what *actually* plays the sound.
+#         stream.write(data)
+#         data = wf.readframes(chunk)
+CHUNK = 1024
+wf = wave.open("/home/demihuman/Documents/project/Tabledrummer/samples/wet hat1.wav",'rb')
+p = pyaudio.PyAudio()
+stream= p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=wf.getnchannels(),rate=wf.getframerate(),output=True)
+data=wf.readframes(CHUNK)
 
-def play(s):
-    if(s=="snare"):
-        print("snare")
-    elif(s=="kick"):
-        print("kick")
-    elif(s=="hat"):
-        print("haaat")
+while len(data)>0:
+   stream.write(data)
+   data=wf.readframes(CHUNK)
 
+stream.stop_stream()
+stream.close()
 
-play("mat")
+p.terminate
+
+# def play(s):
+#     if(s=="snare"):
+#         print("snare")
+#     elif(s=="kick"):
+#         print("kick")
+#     elif(s=="hat"):
+#         print("haaat")
+#
+#
+# play("mat")
